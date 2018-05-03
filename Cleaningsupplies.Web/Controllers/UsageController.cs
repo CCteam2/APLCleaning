1675e36961e256b65eeb4154d385951574fbcb94
judyburns <mystyjb2@earthlink.net>
2018-05-03 11:51:48 -0400

    Added changes for controller and model.

--- Cleaningsupplies.Web/Controllers/UsageController.cs
+++ Cleaningsupplies.Web/Controllers/UsageController.cs
@@ -1,53 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Cleaningsupplies.Web.Models;
+using CleaningSupplies.Database.Models;
+using Microsoft.AspNet.Identity;
 
-namespace EditableWebgrid.Controllers
+namespace Cleaningsupplies.Web.Controllers
 {
     [Authorize]
 
     public class UsageController : Controller
     {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         //
         // GET: /CleaningUsage/
 
         public ActionResult Index()
         {
-            List<UsageModel> items = UsageModel.GetItems();
+            //List<UsageModel> items = UsageModel.GetItems();
+
+            var items = (from m in db.Master select m).ToList();
 
             return View(items);
 
         }
 
-        public JsonResult IncreaseQty(UsageModel model)
+        public JsonResult JsonUpdate(UpdateUsage model)
         {
-            // Update model to your db
-            //  foreach (var item in model) {
-            //      model.ID = item.ID;
-            //      model.Description = item.Description;
-            //      model.QtyAvailable = item.QtyAvailable;
-            //      model.UserQty = item.UserQty;
-            //  }
+            string message = "Update Successful";
 
-            string message = "Success - Increase Quantity - Save Usage";
+            if (model.Quantity_modified == 0)
+            {
+                message = "No Update - Quantity is Zero";
+                return Json(message, JsonRequestBehavior.AllowGet);
+            }
+
+            var userID = db.Users.Find(User.Identity.GetUserId());
+
+            Master master = db.Master.Find(model.ID);
+
+            if (master == null)
+            {
+                message = "Update Failed";
+
+                return Json(message, JsonRequestBehavior.AllowGet);
+            }
+
+            Usage usage = new Usage
+            {
+                Item = model.Description,
+                Quantity_modified = model.Quantity_modified,
+                CreatedByDateTime = DateTime.Now,
+                CreatedById = userID,
+                ModifiedByDatetime = DateTime.Now,
+                ModifiedById = userID,
+                GetMasterT = db.Master.Find(model.ID)
+            };
+
+            db.Entry(usage).State = EntityState.Added;
+
+            db.SaveChanges();
+
+            if (model.Quantity_modified < 0)
+            {
+                message = "Update Successful - Quantity of " + model.Quantity_modified + "Added";
+            }
+            else if (model.Quantity_modified > 0)
+            {
+                message = "Update Successful - Quantity of " + model.Quantity_modified + "Removed";
+            }
+
+            ////compute quantity at hand. Alert user if qty at hand equals or less than alert qty
+            //int qtyOnHand = QueryMethods.SumProductInventoryQuantityForId(model.ID);
+
+            //if (qtyOnHand <= model.AlertThreshHold)
+            //    message = "Update Successful & Alert ThreshHold Met";
+            //else
+            //if (model.Quantity_modified < 0) 
+            //    message = "Update Successful - Quantity of " + model.Quantity_modified + "Added";
+            //else
+            //if (model.Quantity_modified > 0)
+            //    message = "Update Successful - Quantity of " + model.Quantity_modified + "Removed";
+
             return Json(message, JsonRequestBehavior.AllowGet);
-        }
 
-        public JsonResult DecreaseQty(UsageModel model)
-        {
-            // Update model to your db
-            //  foreach (var item in model) {
-            //      model.ID = item.ID;
-            //      model.Description = item.Description;
-            //      model.QtyAvailable = item.QtyAvailable;
-            //      model.UserQty = item.UserQty * -1;
-            //  }
-
-            string message = "Success - Decrease Quantity - Usage";
-            return Json(message, JsonRequestBehavior.AllowGet);
         }
 
     }
---
+++ Cleaningsupplies.Web/Models/UpdateUsage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cleaningsupplies.Web.Models
+{
+    public class UpdateUsage
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        //public int QuantityInStock { get; set; }
+        public int Quantity_modified { get; set; }
+    }
+}
