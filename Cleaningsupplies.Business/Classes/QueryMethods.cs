1cd31e8362b71b0ee25ed48519436003add92d86
judyburns <mystyjb2@earthlink.net>
2018-05-06 15:41:58 -0400

    Updates to Usage View.

---
+++ Cleaningsupplies.Business/Classes/QueryMethods.cs
@@ -0,0 +1,45 @@
+﻿using CleaningSupplies.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cleaningsupplies.Business.Classes
+{
+    public class QueryMethods
+    {
+        public static int GetProdInvSum(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            // get the sum for the quantity in the Usage table using the master id
+            var results = (from p in db.Master
+                           where (p.ID == id)
+                           join pinv in db.Usage on p.ID equals pinv.GetMasterT.ID
+                           orderby p.Description
+                           select pinv).ToList().Select(pinv => pinv.Quantity_modified).Sum();
+
+            return results;
+        }
+
+        // Get: All Masters 
+        public static List<Master> GetAllMasters()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            var result = db.Master;
+
+            return result.ToList();
+        }
+
+        // Get: Master by ID
+        public static Master GetMaster(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            var result = db.Master.Where(x => x.ID == id).FirstOrDefault();
+
+            return result;
+        }
+    }
+}
---
+++ Cleaningsupplies.Web.Tests/Business/QueryMethodsTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cleaningsupplies.Business.Classes;
+using CleaningSupplies.Database.Models;
+using System.Linq;
+
+namespace Cleaningsupplies.Web.Tests.Business
+{
+    [TestClass]
+    public class QueryMethodsTest
+    {
+
+        [TestMethod]
+        public void GetProdInvSum()
+        {
+            //Arrange
+            ApplicationDbContext db = new ApplicationDbContext();
+            int expected = (from p in db.Master
+                            where (p.ID == 2)
+                            join pinv in db.Usage on p.ID equals pinv.GetMasterT.ID
+                            orderby p.Description
+                            select pinv).ToList().Select(pinv => pinv.Quantity_modified).Sum();
+
+            //Act
+            int actual = QueryMethods.GetProdInvSum(2);
+
+            //Assert
+            Assert.AreEqual(expected, expected);
+        }
+
+        [TestMethod]
+        public void GetMaster() 
+        {
+            //Arrange
+            ApplicationDbContext db = new ApplicationDbContext();
+            Master expected = db.Master.Where(x => x.ID == 2).FirstOrDefault(); ;
+
+            //Act
+            Master actual = QueryMethods.GetMaster(2);
+
+            //Assert
+            Assert.AreSame(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetAllMasters()
+        {
+            //Arrange
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<Master> expected = db.Master.ToList();
+
+            //Act
+            List<Master> actual = QueryMethods.GetAllMasters();
+
+            //Assert
+            Assert.ReferenceEquals(expected, actual);
+        }
+    }
+}
