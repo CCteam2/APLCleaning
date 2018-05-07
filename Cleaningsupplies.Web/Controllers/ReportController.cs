35e6ce82b18994fee959997e192700904e0ca53c
mallanc <33516799+mallanc@users.noreply.github.com>
2018-05-07 10:55:58 -0400

    getting latest

--- Cleaningsupplies.Web/Controllers/ReportController.cs
+++ Cleaningsupplies.Web/Controllers/ReportController.cs
@@ -10,7 +10,11 @@ using CleaningSupplies.Database.Models;
 
 namespace Cleaningsupplies.Web.Controllers
 {
+    [Authorize]
+
     public class ReportController : Controller
+
+
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
